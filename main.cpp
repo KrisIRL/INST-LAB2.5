3b0bb317430247958094e36110a5bd25abfb8610
KrisIRL <katshkins@gmail.com>
2023-04-22 11:51:36 +0000

    INST LAB-Q2.5 Thread 5 added
    Signed-off-by: KrisIRL <katshkins@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,4 +1,11 @@
-// example to test the mbed Lab Board lcd lib with the mbed rtos
+// This is a sample program to demonstrate the use of the mbed Lab Board LCD library with the mbed RTOS.
+// The program creates 5 threads that perform different tasks:
+// 1. Thread 1: Prints a counter in the first line of the LCD and increments it every 1 second.
+// 2. Thread 2: Prints another counter in the third line of the LCD and increments it every 0.5 seconds.
+// 3. Thread 3: Displays a sine wave on a small window on the LCD, with the sine wave's speed controlled by Pot1.
+// 4. Thread 4: Reads the value of Pot2 and sets the LCD contrast accordingly.
+// 5. Thread 5: Reads the state of the joystick and the fire button and sets the corresponding LEDs.
+// The program uses mutexes to ensure that the LCD is accessed by only one thread at a time, preventing any conflicts or data corruption.
 // Pot1 change the contrast
 // Pot2 change the speed of the sin wave
 
@@ -10,12 +17,17 @@
 #include "stdio.h"
 #include "C12832_lcd.h"
 
+// joystick
+BusIn joy(p15,p12,p13,p16);
+DigitalIn fire(p14);
+
+BusOut leds(LED1,LED2,LED3,LED4);
+
 // LCD object
 C12832_LCD LCD;
 
 AnalogIn Pot1(p19);
 AnalogIn Pot2(p20);
-
 
 // mutex to make the lcd lib thread save
 Mutex lcd_mutex;
@@ -85,7 +97,7 @@ void thread4(void const *args)
 {
     int k;
     while(true) {         // thread loop
-    k = Pot2.read_u16();  // get the value of poti 2
+    k = Pot2.read_u16();  // get the value of pot 2
     k = k >> 10;          // we need only 6 bit for contrast
      lcd_mutex.lock();
     LCD.set_contrast(k);
@@ -94,10 +106,26 @@ void thread4(void const *args)
 }
 }
 
+// Thread 5
+void thread5(void const *args){
+       while(1) {
+        if (fire) {
+            leds=0xf;
+        } else {
+            leds=joy;
+        }
+        wait(0.1);
+    }
+    
+    }
+
 
 // print the actual contrast 
-int main()
-{
+int main(){
+    
+    
+    Thread t5(thread5); //start thread5
+    
     int j;
     LCD.cls();
   
